d4bb61427ae1218a490e9109630c63e9b2fa70ff
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2020-09-04 23:19:55 +0100

    Basic 3 page layout for Vuetify app

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -6,8 +6,26 @@
       :floating="false"
       :mini-variant="false"
       app
+      :expand-on-hover="true"
       overflow
-    ></v-navigation-drawer>
+    >
+      <v-list>
+        <v-list-item
+          v-for="item in primaryDrawer.items"
+          :key="item.title"
+          link
+          :to="item.link"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-app-bar :clipped-left="false" app>
       <v-app-bar-nav-icon
@@ -24,11 +42,21 @@
       <span class="px-4"
         >&copy; {{ new Date().getFullYear() }} Daniel Tomlinson</span
       >
+      <v-spacer />
+      <span class="px-4">
+        <v-icon>{{ icons.mdiGithub }}</v-icon> View on GitHub
+      </span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import {
+  mdiViewDashboard,
+  mdiCalculator,
+  mdiHelpBox,
+  mdiGithub,
+} from "@mdi/js";
 
 export default {
   name: "App",
@@ -36,6 +64,14 @@ export default {
   data: () => ({
     primaryDrawer: {
       model: true,
+      items: [
+        { title: "Home", icon: mdiViewDashboard, link: "/" },
+        { title: "Help", icon: mdiHelpBox, link: "help" },
+        { title: "Calculator", icon: mdiCalculator, link: "calculator" },
+      ],
+    },
+    icons: {
+      mdiGithub: mdiGithub,
     },
   }),
 };
--- calculator/src/views/HelloWorld.vue
+++ calculator/src/views/HelloWorld.vue
@@ -19,60 +19,11 @@
           Calculator for Anno 1800.
         </p>
       </v-col>
-
-      <!-- <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
+    </v-row>
+    <v-row class="text-center">
+      <v-col cols="12">
+        What next?
       </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col> -->
     </v-row>
   </v-container>
 </template>
