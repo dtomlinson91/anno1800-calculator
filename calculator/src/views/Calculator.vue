70cb52a403337cab823e076d21ea970e81be2e30
Daniel Tomlinson <dtomlinson@panaetius.co.uk>
2020-09-08 00:05:39 +0100

    Updating latest

--- calculator/src/views/Calculator.vue
+++ calculator/src/views/Calculator.vue
@@ -35,10 +35,27 @@
           hide-default-footer
           dense
         >
-
-        <template v-slot:[`item.electricity_supplied`]="{ item }">
-          <v-simple-checkbox v-model="item.electricity_supplied" :disabled="!item.improved_by_electricity"></v-simple-checkbox>
-        </template>
+          <template v-slot:[`item.number_needed`]="{ item }">
+            <v-edit-dialog :return-value.sync="item.number_needed" large>
+              <div class="font-italic">{{ item.number_needed }}</div>
+              <template v-slot:input>
+                <div class="mt-4">Number of final buildings needed.</div>
+                <v-text-field
+                  v-model="item.number_needed"
+                  label="Edit"
+                  single-line
+                >
+                </v-text-field>
+              </template>
+            </v-edit-dialog>
+          </template>
+          <template v-slot:[`item.electricity_supplied`]="{ item }">
+            <v-simple-checkbox
+              v-model="item.electricity_supplied"
+              :disabled="!item.improved_by_electricity"
+              :ripple="false"
+            ></v-simple-checkbox>
+          </template>
         </v-data-table>
       </v-col>
     </v-row>
@@ -78,12 +95,40 @@ export default {
       for (let i = 0; i < val.length; i++) {
         for (let j = 0; j < this.initial_data.raw.length; j++) {
           if (val[i] === this.initial_data.raw[j].chain) {
+            //  create initial table data
             new_data.push(this.initial_data.raw[j]);
+            //  insert the defaults
           }
+        }
+      }
+      for (let i = 0; i < new_data.length; i++) {
+        if (new_data[i].final_building === true) {
+          new_data[i].number_needed = 1;
         }
       }
       this.calculator_data.raw = new_data;
     },
+    // calculator_data: {
+    // deep: true,
+    // handler(val) {
+    //   // Set the initial number needed for final buildings
+    //   for (let i = 0; i < val.raw.length; i++) {
+    //     console.log(typeof val.raw[i].number_needed === "undefined");
+    //     if (
+    //       val.raw[i].final_building === true &&
+    //       typeof val.raw[i].number_needed === "undefined"
+    //     ) {
+    //       val.raw[i].number_needed = 1;
+    //       console.log("resetting to one");
+    //     } else if (
+    //       val.raw[i].final_building === false &&
+    //       typeof val.raw[i].number_needed === "undefined"
+    //     ) {
+    //       val.raw[i].number_needed = "N/A";
+    //     }
+    //   }
+    // },
+    // },
   },
 };
 </script>
